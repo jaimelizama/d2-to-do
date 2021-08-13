fa46d43116bac31337692d451c5a3db2021f990c
Jaime Lizama R <jlizama@poptimize.cl>
2021-08-12 23:45:31 -0400

    ToDo List

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container">
+    <h2 class="my-4">Crea una nueva tarea</h2>
+    <label class="mr-2">Tarea:</label>
+    <input type="text" placeholder="Ingrese nueva tarea" v-model="newToDo" />
+    <button v-on:click="createToDo()">Crear</button>
+    <h3 class="my-4">Lista de Tareas</h3>
+    <ul>
+      <li v-for="(ToDo, $index) in existingToDo" :key="$index">
+        {{ $index + 1 }}: {{ ToDo.text }}
+      </li>
+    </ul>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    newToDo: '',
+    existingToDo: []
+  }),
+  methods: {
+    createToDo() {
+      this.existingToDo.push({ text: this.newToDo }), (this.newToDo = '')
+    }
   }
 }
 </script>
@@ -19,10 +30,13 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 400px;
+}
+
+li {
+  text-align: left;
 }
 </style>
